69ac38fe2cfd45b0301a5fb2e2814dda73860eb7
Matthew Giardina <mlgiardina@me.com>
2015-06-16 15:49:12 -0500

    finished fizzbuzz for js and rb

---
+++ js/fizzBuzz.js
@@ -0,0 +1,18 @@
+var fizzbuzz = function(num) {
+  for (var i = 0; i <= num; i ++) {
+    if (i % 15 == 0) {
+      console.log("fizzbuz");
+    }
+    else if (i % 5 == 0) {
+      console.log("buzz");
+    }
+    else if (i % 3 == 0) {
+      console.log("fizz");
+    }
+    else {
+      console.log(i);
+    }
+  };
+}
+
+fizzbuzz(100)
--- rb/fizzbuzz.rb
+++ rb/fizzbuzz.rb
@@ -1,6 +1,15 @@
 def fizzbuzz(num)
-  fb(num)
-  fizz(num)
-  buzz(num)
+  (1..num).each do |n|
+    if n % 15 == 0
+      puts "fizzbuzz"
+    elsif n % 5 == 0
+      puts "buzz"
+    elsif n % 3 == 0
+      puts "fizz"
+    else
+      puts n
+    end
+  end
 end
 
+fizzbuzz(100)
